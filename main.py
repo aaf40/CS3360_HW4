5218c546518a2117a9bb7585daab92b0083e34e8
aaf40 <aaf40@txstate.edu>
2024-03-26 16:33:48 -0500

    Version that only takes args 10 and 0.04 to satisfy project requirements

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 # To run use command python main.py <lambda_rate> <average_service_time>
-# Where <lambda_rate> is between 10 and 30 and <average_service_time> is between 0.01 and 0.04.
+# Where <lambda_rate> and <average_service_time> must be 10 and 0.04 respectively.
 import sys
 from simulator import Simulator
 
 def main():
     if len(sys.argv) != 3:
-        print("Usage: python main.py <lambda_rate> <average_service_time>")
+        print("Incorrect Usage. Valid input contains exactly 3 arguments as follows:")
+        print("Correct Usage: python main.py <lambda_rate> <average_service_time>")
+        print("Correct Usage: python main.py 10 0.04")
         sys.exit(1)
 
     try:
         lambda_rate = float(sys.argv[1])
         average_service_time = float(sys.argv[2])
 
-        if lambda_rate < 10 or lambda_rate > 30 or average_service_time < 0.01 or average_service_time > 0.04:
+        if lambda_rate != 10 or average_service_time != 0.04:
             raise ValueError
     
     except ValueError:
-        print("Invalid input. <lambda_rate> must be between 0 and 30 and <average_service_time> between 0.01 and 0.04.")
+        print("Invalid input. <lambda_rate> and <average_service_time>" +
+        " must be 10 and 0.04 respectively.")
         sys.exit(1)
 
-    simulator = Simulator(lambda_rate, average_service_time)
-    simulator.run()
+    while lambda_rate < 31:
+        print("Results for lambda rate of " + str(lambda_rate) + ":")
+        simulator = Simulator(lambda_rate, average_service_time)
+        simulator.run()
+        lambda_rate += 1
+        print()
 
 if __name__ == "__main__":
     main()
